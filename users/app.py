0aa85da9a6d66fbd93b91b8c1182ffec06481a01
Gabe Mukobi <gabrielmukobi@google.com>
2019-07-08 15:49:37 -0700

    Add basic dummy user authorization routing

--- users/app.py
+++ users/app.py
@@ -1,12 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from the users service!'
+
+@app.route('/v1/users/authorization', methods=['GET'])
+def find_authorization():
+    user = request.args.get('user_id')
+    if user == None:
+        return jsonify(error="You must supply a 'user_id' parameter!")
+    else:
+        authorized = user != 'Voldemort'
+        return jsonify(edit_access=authorized)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
