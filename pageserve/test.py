c631e3389cc7a276cff7f46e542585499ed3bb6f
Carolyn Mei <mcarolyn@google.com>
2019-07-08 17:41:59 -0700

    cleaned up comments

--- pageserve/test.py
+++ pageserve/test.py
@@ -1,14 +1,10 @@
 import unittest
 from app import *
 
-# currently not working
 class TestServe(unittest.TestCase):
-    def test1(self):
+    def test_get_auth(self):
         self.assertTrue(get_auth("carolyn"))
         self.assertFalse(get_auth("Voldemort"))
-        
-    def test2(self):
-        pass
         
         
 if __name__ == '__main__':
