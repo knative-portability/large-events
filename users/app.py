6119548c9a9d2bd6d8daad8e1b14cc2fdf97d96e
Gabe Mukobi <gabrielmukobi@google.com>
2019-07-09 11:42:48 -0700

    Format users app

--- users/app.py
+++ users/app.py
@@ -12,6 +12,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)  # pylint: disable=invalid-name
 
+
 @app.route('/v1/authorization', methods=['GET'])
 def get_authorization():
     """Finds whether the given user is authorized for edit access"""
@@ -21,10 +22,12 @@ def get_authorization():
     authorized = is_authorized_to_edit(user, None)
     return jsonify(edit_access=authorized)
 
+
 def is_authorized_to_edit(user, database):
     """Queries the db to find authorization of the given user"""
     # TODO(mukobi) query db for actual user authorization
     return user != 'Voldemort'
+
 
 @app.route('/v1/', methods=['PUT'])
 def add_update_user():
@@ -40,6 +43,7 @@ def add_update_user():
         "user_id": "0", "username": "Dummy User", "edit_access": False
     }
     return jsonify(user_object), (201 if added_new_user else 200)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
