db674801a272856bf75c4c34ea7da6ccb47de9e3
Gabe Mukobi <gabrielmukobi@google.com>
2019-07-10 13:21:39 -0700

    Improve user auth test fixture and cleanup

--- users/test.py
+++ users/test.py
@@ -13,32 +13,43 @@ class TestAuthorization(unittest.TestCase):
 
     def setUp(self):
         """Seed DB for testing"""
+        self.id_of_test_user_is_organizer = "test-is-organizer-12345"
+        self.id_of_test_user_not_organizer = "test-not-organizer-12345"
         # update or insert (upsert) a new user
         app.DB.users.update(
-            {"username": "mukobi"},
-            {"username": "mukobi",
-             "name": "Gabriel Mukobi",
+            {"username": self.id_of_test_user_is_organizer},
+            {"username": self.id_of_test_user_is_organizer,
+             "name": self.id_of_test_user_is_organizer,
              "is_organizer": True},
             upsert=True)
         app.DB.users.update(
-            {"username": "foobar"},
-            {"username": "foobar",
-             "name": "Unauthorized User",
+            {"username": self.id_of_test_user_not_organizer},
+            {"username": self.id_of_test_user_not_organizer,
+             "name": self.id_of_test_user_not_organizer,
              "is_organizer": False},
             upsert=True)
 
     def test_good_user_is_authorized(self):
         """An authorized user should have authorized privileges"""
-        self.assertTrue(app.is_authorized_to_edit("mukobi"))
+        self.assertTrue(app.is_authorized_to_edit(
+            self.id_of_test_user_is_organizer))
 
     def test_bad_user_not_authorized(self):
         """An non-authorized user should not have authorized privileges"""
-        self.assertFalse(app.is_authorized_to_edit("foobar"))
+        self.assertFalse(app.is_authorized_to_edit(
+            self.id_of_test_user_not_organizer))
 
     def test_unkown_user_not_authorized(self):
         """An user not found in the db should not have authorized privileges"""
         self.assertFalse(app.is_authorized_to_edit(
             "notAValidUsername31415926"))
+
+    def tearDown(self):
+        """Clean up test users"""
+        app.DB.users.delete_many(
+            {"$or": [
+                {"username": self.id_of_test_user_is_organizer},
+                {"username": self.id_of_test_user_not_organizer}]})
 
 
 if __name__ == '__main__':
