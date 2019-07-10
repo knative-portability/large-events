0c04207cf663b107d76511274ba53bb198746c03
Carolyn Mei <mcarolyn@google.com>
2019-07-10 15:50:08 -0700

    set up connection to MongoDB

--- events/app.py
+++ events/app.py
@@ -1,10 +1,25 @@
 import os
 import logging
-import requests
+import pymongo
+# import pprint
 
 from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
+
+# connect to MongoDB Atlas database
+MONGODB_ATLAS_USERNAME = os.environ.get(
+    "MONGODB_ATLAS_USERNAME")
+MONGODB_ATLAS_PASSWORD = os.environ.get(
+    "MONGODB_ATLAS_PASSWORD")
+MONGODB_ATLAS_CLUSTER_ADDRESS = os.environ.get(
+    "MONGODB_ATLAS_CLUSTER_ADDRESS")
+PYMONGO_URI = "mongodb+srv://{}:{}@{}".format(
+        MONGODB_ATLAS_USERNAME,
+        MONGODB_ATLAS_PASSWORD,
+        MONGODB_ATLAS_CLUSTER_ADDRESS)
+print("Pymongo URI: {}".format(PYMONGO_URI))
+DB = pymongo.MongoClient(PYMONGO_URI).test
 
 
 @app.route('/v1/add', methods=['POST'])
@@ -27,6 +42,6 @@ def search_event():
     pass
 
 
-@app.route('/v1/<event-id>', methods=['PUT'])
-def get_one_event():
+@app.route('/v1/<event_id>', methods=['PUT'])
+def get_one_event(event_id):
     pass
