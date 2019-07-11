e89282927f626a42c14d1d6ce5195c0b2f3c0011
Gabe Mukobi <gabrielmukobi@google.com>
2019-07-10 17:32:57 -0700

    Add posts endpoints and basic docstrings outlining functionality

--- posts/app.py
+++ posts/app.py
@@ -28,5 +28,39 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+@app.route('/v1/add', methods=['POST'])
+def upload_new_post():
+    """
+    Make a new post upload to the server.
+
+    Post request body should contain:
+    event_id: id of the event to post to
+    author_id: user id of the user making the post
+    text: text to be sent
+    Post request files should contain all the files the user wants to upload to the server.
+    """
+
+
+@app.route('/v1/', methods=['GET'])
+def get_all_posts():
+    """
+    Get all posts for the whole event
+    """
+
+
+@app.route('/v1/<post_id>', methods=['GET'])
+def get_post_by_id(post_id):
+    """
+    Get the post with the specified ID
+    """
+
+
+@app.route('/v1/by_event/<event_id>', methods=['GET'])
+def get_all_posts_for_event(event_id):
+    """
+    Get all posts matching the event with the specified ID
+    """
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
