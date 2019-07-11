092c62921b9437d30b04e5042443eda95670d126
Carolyn Mei <mcarolyn@google.com>
2019-07-11 14:24:30 -0700

    clean up code

--- events/test.py
+++ events/test.py
@@ -5,7 +5,8 @@ import app
 
 class TestEventsDB(unittest.TestCase):
     def setUp(self):
-        # TODO(cmei4444): move this to a mock DB to avoid the official DB interfering with tests
+        # TODO(cmei4444): move this to a mock DB to avoid the official DB
+        # interfering with tests
         self.example_time = datetime.datetime(2019, 6, 11, 10, 33, 1, 100000)
         self.event_info = {'name': 'test_event',
                            'description': 'testing!',
@@ -13,7 +14,8 @@ class TestEventsDB(unittest.TestCase):
                            'event_time': '7-12-2019',
                            'created_at': self.example_time}
         self.test_event = app.Event(self.event_info)
-        app.events_coll.delete_many(self.event_info)    # make sure DB has no other test entries
+        # make sure DB has no other test entries
+        app.events_coll.delete_many(self.event_info)
 
     def test_add(self):
         self.test_event.add_to_db()
@@ -34,7 +36,8 @@ class TestEventsDB(unittest.TestCase):
         self.assertEqual(info['created_at'], time)
 
     def tearDown(self):
-        app.events_coll.delete_many(self.event_info)    # empty DB of test entries
+        app.events_coll.delete_many(
+            self.event_info)    # empty DB of test entries
 
 
 class TestEventsClass(unittest.TestCase):
