58c2d85d1a5244b68b984e9e2baa2ec55f981631
Carolyn Mei <mcarolyn@google.com>
2019-07-17 11:11:36 -0700

    Remove outdated pageserve test - unrelated to events

--- pageserve/test.py
+++ pageserve/test.py
@@ -16,11 +16,12 @@ limitations under the License.
 import unittest
 import app
 
+
 class TestServe(unittest.TestCase):
     def test_get_auth(self):
-        self.assertTrue(app.get_auth("carolyn"))
-        self.assertFalse(app.get_auth("Voldemort"))
-        
-        
+        # TODO: test authorization retrieval
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
