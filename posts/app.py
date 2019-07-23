4000202c4c42a327ab14761ea5c3ad6a97f79d96
Gabe Mukobi <gabrielmukobi@google.com>
2019-07-23 14:46:20 -0700

    Correctly serve posts as json with "posts" key

--- posts/app.py
+++ posts/app.py
@@ -20,9 +20,10 @@
 
 import os
 import uuid
+import json
 import pymongo
 from bson import json_util, ObjectId
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from werkzeug.exceptions import BadRequestKeyError
 from google.cloud import storage
 
@@ -62,24 +63,26 @@ def upload_new_post():
 def get_all_posts():
     """Get all posts for the whole event."""
     # serialize otherwise nonserializable ObjectIDs
-    return json_util.loads(json_util.dumps(
-        find_posts_in_db(POSTS_COLLECTION)))
+    return json.loads(json_util.dumps(
+        {"posts": find_posts_in_db(POSTS_COLLECTION)}))
 
 
 @app.route('/v1/<post_id>', methods=['GET'])
 def get_post_by_id(post_id):
     """Get the post with the specified ID."""
     # serialize otherwise nonserializable ObjectIDs
-    return json_util.loads(json_util.dumps(
-        find_posts_in_db(POSTS_COLLECTION, post_id=ObjectId(post_id))))
+    return json.loads(json_util.dumps(
+        {"posts": find_posts_in_db(
+            POSTS_COLLECTION, post_id=ObjectId(post_id))}))
 
 
 @app.route('/v1/by_event/<event_id>', methods=['GET'])
 def get_all_posts_for_event(event_id):
     """Get all posts matching the event with the specified ID."""
     # serialize otherwise nonserializable ObjectIDs
-    return json_util.loads(json_util.dumps(
-        find_posts_in_db(POSTS_COLLECTION, event_id=event_id)))
+    return json.loads(json_util.dumps(
+        {"posts": find_posts_in_db(
+            POSTS_COLLECTION, event_id=event_id)}))
 
 
 def find_posts_in_db(collection, post_id=None, event_id=None):
