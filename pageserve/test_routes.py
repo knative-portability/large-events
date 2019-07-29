ecce869cdfe582a5871fe7671cf7dda593f5919b
Carolyn Mei <mcarolyn@google.com>
2019-07-29 14:18:55 -0700

    Set up flask_testing for testing render_templates

--- pageserve/test_routes.py
+++ pageserve/test_routes.py
@@ -15,15 +15,30 @@ limitations under the License.
 
 import unittest
 from unittest.mock import patch
-import app
+from flask import Flask
+from flask_testing import TestCase
+from app import app
 
 
-class TestRoutes(unittest.TestCase):
+class TestRoutes(TestCase):
+    def create_app(self):
+        """Creates and returns a Flask instance.
+
+        Required by flask_testing to test templates."""
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+
     def setUp(self):
         """Set up test client."""
-        self.client = app.APP.test_client()
+        self.client = app.test_client()
 
     def test_index(self):
         """Check that index page is rendered correctly."""
-        self.client.get('/v1/')
-        self.assert_template_used('index.html')
+        response = self.client.get('/v1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_show_events(self):
+        """Check that sub-events page is rendered correctly."""
+        response = self.client.get('/v1/events')
+        self.assertEqual(response.status_code, 200)
