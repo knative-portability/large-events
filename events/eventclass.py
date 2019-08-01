c537e1c30feeed5453a70b5ff0375f69c2588a8c
Carolyn Mei <mcarolyn@google.com>
2019-08-01 14:32:18 -0700

    Remove unneeded conversion to dict

--- events/eventclass.py
+++ events/eventclass.py
@@ -68,6 +68,6 @@ class Event(namedtuple("EventTuple", EVENT_ATTRIBUTES)):
         if info['event_id']:    # only create _id field if event_id is not None
             info['_id'] = info['event_id']
             del info['event_id']
-        return dict(info)
+        return info
 
     dict = property(get_dict)
